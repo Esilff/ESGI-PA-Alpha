2b625895c47820691471b05fb659a81b7793dc8b
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2023-03-20 11:12:00 +0100

    menu debut fin et controller

--- src/ESGI-PA/Assets/Scripts/MenuController.cs
+++ src/ESGI-PA/Assets/Scripts/MenuController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class menufin : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
       public void RestartGame()
     {

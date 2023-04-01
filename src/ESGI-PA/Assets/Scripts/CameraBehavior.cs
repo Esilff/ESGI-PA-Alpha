53fd307baaa7066629811a3674e87ccbbe425de3
LaBriquez <72671532+LaBriquez@users.noreply.github.com>
2023-04-01 14:43:29 +0200

    Update CameraBehavior.cs

--- src/ESGI-PA/Assets/Scripts/CameraBehavior.cs
+++ src/ESGI-PA/Assets/Scripts/CameraBehavior.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    [SerializeField] private static Transform target;
+    private static Transform target;
     [SerializeField] private Transform camera;
 
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 offset, position;
 
-    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float cameraSpeed, lerpSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +16,21 @@ public class CameraBehavior : MonoBehaviour
         camera.rotation = Quaternion.Slerp(camera.rotation, target.rotation, cameraSpeed);
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
         //if (Physics.Raycast(camera.position, -camera.up, 2f)) offset.y = 1;
-    }
+    }*/
 
-    // Update is called once per frame
-    private void LateUpdate()
+    private void Update()
     {
-        camera.position = target.position + (target.forward * offset.z) + Vector3.up * offset.y;
-        camera.LookAt(target, target.up);
+        position = Vector3.Lerp(position, target.position + (target.forward * offset.z) + Vector3.up * offset.y, lerpSpeed * Time.deltaTime);
+        camera.rotation = Quaternion.Lerp(camera.rotation, Quaternion.LookRotation(target.position - camera.position), lerpSpeed * Time.deltaTime);
+        
+        if (Physics.Raycast(target.position, position - target.position,
+                out RaycastHit hit, Vector3.Distance(position, target.position)))
+            camera.position = Vector3.Lerp(hit.point, target.position, 0.2f);
+        else
+            camera.position = position;
     }
 
     public static void setTarget(Transform obj)

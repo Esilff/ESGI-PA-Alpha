1b0f797591abe49d44db566260390f09c812834b
LaBriquez <72671532+LaBriquez@users.noreply.github.com>
2023-05-01 15:06:23 +0200

    Update MainCar.cs

--- src/ESGI-PA/Assets/NewPlayer/MainCar.cs
+++ src/ESGI-PA/Assets/NewPlayer/MainCar.cs
@@ -6,36 +6,56 @@ public class MainCar : MonoBehaviour
     [SerializeField] Vector3 carOffset;
     public Vector3 CarOffset => carOffset;
 
-    [SerializeField] float carSize, camSpeed;
-    public float CarSize => carSize;
+    [SerializeField] float camSpeed;
     [SerializeField] Vector3 camOffset;
     public Vector3 CamOffset => camOffset;
 
-    [SerializeField] Vector3 W1, W2, W3, W4;
-    [SerializeField] Rigidbody rb;
-    [SerializeField] float accelerationForce, rotForce;
+    [SerializeField] CharacterController controller;
+    [SerializeField] float accelerationForce, gravity, maxSpeed, frictionForce, rotForce, rotationLerp;
+
+    [SerializeField] Vector3 normalAngle;
+
+    void Start()
+    {
+        controller.Move(Vector3.down);
+    }
 
     public void CameraMovement(Camera cam)
     {
         cam.transform.position = Vector3.Lerp(cam.transform.position, transform.TransformPoint(camOffset), camSpeed * Time.deltaTime);
+
         cam.transform.LookAt(transform.position);
     }
 
-    bool WheelCollide(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
-    {
-        return true;
-    }
-    
     public void Move(PlayerInput input)
     {
+        Vector3 movement = Vector3.zero;
+
         Vector2 move = input.actions["Move"].ReadValue<Vector2>();
 
-        if (WheelCollide(transform.TransformPoint(W1), transform.TransformPoint(W2), transform.TransformPoint(W3),
-                transform.TransformPoint(W4)))
+        if (controller.isGrounded)
         {
-            rb.AddForce(move.y * Time.deltaTime * accelerationForce * transform.forward, ForceMode.Acceleration);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.up, normalAngle) * transform.rotation, rotationLerp * Time.deltaTime);
+            float zSpeed = transform.InverseTransformDirection(controller.velocity).z;
+            movement = transform.forward *
+                (zSpeed * Time.deltaTime +
+                 move.y * accelerationForce * (1 - zSpeed / maxSpeed) * Time.deltaTime - Mathf.Sign(zSpeed) * frictionForce * Time.deltaTime);
+
+            if (Mathf.Abs(zSpeed) > 0.5f)
+                transform.Rotate(transform.up, move.x * Mathf.Sign(zSpeed) * rotForce * Time.deltaTime);
+        }
+        else
+        {
+            movement = controller.velocity * Time.deltaTime;
         }
         
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, Time.deltaTime * rotForce * move.x, 0f));
+        movement.y -= gravity * Time.deltaTime;
+
+        controller.Move(movement);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        normalAngle = hit.normal;
     }
 }
